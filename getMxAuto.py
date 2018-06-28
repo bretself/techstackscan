8192ce01ffe10572e8ea547f5eb2d14d55400ba7
Bret Self <bretself@gmail.com>
2018-06-28 17:22:11 -0400

    Remove internal graphml writer. Use networkX.

--- getMxAuto.py
+++ getMxAuto.py
@@ -1,4 +1,5 @@
 import dns.resolver
+import networkx as nx
 import sys
 
 def main():
@@ -34,8 +35,8 @@ def main():
     
     if isWriteGraphml:
         print('Writing graphml file')
-        graphmlLines = convertToGraphmlLines(domainToMxSetMap)
-        writeLinesToFile(graphmlOutputFilename, graphmlLines)
+        diGraph = getDirectedGraph(domainToMxSetMap)
+        convertToGraphmlLines(diGraph, graphmlOutputFilename)
     
     if isWriteCsv:
         print('Writing csv file')
@@ -56,156 +57,9 @@ def writeLinesToFile(filename, lines):
         outputFile.write(line + '\n')
         
     outputFile.close()
-
-def convertToGmlLines(domainToMxSetMap):
-    #graph [
-    #        comment "This is a sample graph"
-    #        directed 1
-    #        id n42
-    #        label "Hello, I am a graph"
-    #        node [
-    #                id 1
-    #                label "node 1"
-    #                thisIsASampleAttribute 42
-    #        ]
-    #        node [
-    #                id 2
-    #                label "node 2"
-    #                thisIsASampleAttribute 43
-    #        ]
-    #        node [
-    #                id 3
-    #                label "node 3"
-    #                thisIsASampleAttribute 44
-    #        ]
-    #        edge [
-    #                source 1
-    #                target 2
-    #                label "Edge from node 1 to node 2"
-    #        ]
-    #        edge [
-    #                source 2
-    #                target 3
-    #                label "Edge from node 2 to node 3"
-    #        ]
-    #        edge [
-    #                source 3
-    #                target 1
-    #                label "Edge from node 3 to node 1"
-    #        ]
-    #]
     
-    lines = []
-    
-    #Start new graph
-    lines.append('graph [')
-    lines.append('\tdirected 1')
-    lines.append('\tid 001')
-    lines.append('\tlabel "MX Graph"')
-    
-    #Create nodes
-    #        node [
-    #                id 1
-    #                label "node 1"
-    #                thisIsASampleAttribute 42
-    #        ]
-    
-    domainSet = set()
-    domainSet.update(sorted(domainToMxSetMap.keys()))
-        
-    #MX domain nodes
-    for domain,mxSet in domainToMxSetMap.items():
-        domainSet.update(mxSet)
-    
-    for domain in domainSet:
-        lines.append('\tnode [')
-        lines.append('\t\tid "'+domain.replace('.','')+'"')
-        lines.append('\t\tlabel "'+domain+'"')
-        lines.append('\t]')
-    
-    #Create edges
-    #        edge [
-    #                source 3
-    #                target 1
-    #                label "Edge from node 3 to node 1"
-    #        ]
-    for domain,mxSet in domainToMxSetMap.items():
-    
-        idIndex = 0
-        for mxDomain in mxSet:
-            lines.append('\tedge [')
-            lines.append('\t\tsource "'+domain.replace('.','')+'"')
-            lines.append('\t\ttarget "'+mxDomain.replace('.','')+'"')
-            lines.append('\t]')
-            idIndex += 1
-    
-    #End graph
-    lines.append(']')
-    
-    return lines
-    
-def convertToGraphmlLines(domainToMxSetMap):
-    
-    lines = []
-
-    #<?xml version="1.0" encoding="UTF-8"?>
-    #<graphml xmlns="http://graphml.graphdrawing.org/xmlns"  
-    #    xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance"
-    #    xsi:schemaLocation="http://graphml.graphdrawing.org/xmlns/1.0/graphml.xsd">
-    #  <graph id="G" edgedefault="undirected">
-    #    <node id="n0"/>
-    #    <node id="n1"/>
-    #    <edge id="e1" source="n0" target="n1"/>
-    #  </graph>
-    #</graphml>
-    
-    #print graphml header
-    lines.append('<?xml version="1.0" encoding="UTF-8"?>')
-    lines.append('<graphml xmlns="http://graphml.graphdrawing.org/xmlns"')
-    lines.append('    xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance"')
-    lines.append('    xsi:schemaLocation="http://graphml.graphdrawing.org/xmlns/1.0/graphml.xsd">')
-    
-    #Add shape definitions
-    lines.append('<key id="shape" for="node" attr.name="shape" attr.type="string"/>')
-    
-    #Start new graph
-    lines.append('  <graph id="G" edgedefault="directed">')
-    
-    #Create nodes
-    
-    domainSet = set()
-    domainKeys = sorted(domainToMxSetMap.keys())
-    domainSet.update(domainKeys)
-        
-    #MX domain nodes
-    for domain,mxSet in domainToMxSetMap.items():
-        domainSet.update(mxSet)
-    
-    for domain in domainSet:
-        lines.append('\t<node id="'+domain+'">')
-        
-        shape = 'RECTANGLE'
-        if domain in domainKeys:
-            shape = 'ELLIPSE'
-        
-        lines.append('\t\t<data key="shape">'+shape+'</data>')
-        lines.append('\t</node>')
-    
-    #Create edges
-    for domain,mxSet in domainToMxSetMap.items():
-    
-        idIndex = 0
-        for mxDomain in mxSet:
-            lines.append('\t<edge id="'+ domain + str(idIndex)+'" source="'+domain+'" target="'+mxDomain+'" />')
-            idIndex += 1
-    
-    #End graph
-    lines.append('  </graph>')
-    
-    #End graphml file
-    lines.append('</graphml>')
-    
-    return lines
+def convertToGraphmlLines(directedGraph, graphmlFilename):
+    nx.write_graphml(directedGraph, graphmlFilename)
     
 def convertToCsvLines(domainToMxSetMap):
   
@@ -219,6 +73,29 @@ def convertToCsvLines(domainToMxSetMap):
             lines.append(domain + ',' + mxDomain)
   
     return lines
+
+def getDirectedGraph(domainToMxSetMap):
+
+    DG=nx.DiGraph()
+    
+    #Add nodes
+    domainSet = set()
+    domainKeys = sorted(domainToMxSetMap.keys())
+    domainSet.update(domainKeys)
+        
+    #MX domain nodes
+    for domain,mxSet in domainToMxSetMap.items():
+        domainSet.update(mxSet)
+    
+    for domain in domainSet:
+        DG.add_node(domain)
+    
+    #Add edges
+    for domain,mxSet in domainToMxSetMap.items():
+        for mxDomain in mxSet:
+            DG.add_edge(domain,mxDomain)
+            
+    return DG
     
 def getMxMapping(domainsFilename):
 
